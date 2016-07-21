144c284d54d6839abb865f8127ef7ae751ec181f
Edwin Neef <edwinneef@gmail.com>
2016-07-21 14:24:44 +0000

    Add admin rights

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user and !current_user.admin?
       flash[:danger] = "Je kan alleen je eigen artikelen bewerken/verwijderen lamlul"
       redirect_to root_path
     end
